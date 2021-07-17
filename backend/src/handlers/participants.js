b024b5ff141f2e163a167435d62689b221aec0a7
pleonardodp12 <pauloleonardo_dp@outlook.com>
2021-07-17 19:03:48 -0300

    create post new participant in event, create delete participant in event

--- backend/src/handlers/participants.js
+++ backend/src/handlers/participants.js
@@ -1,8 +1,45 @@
+const { v4: uuidv4 } = require('uuid')
+
+require('../resources/db/connection')()
+
+const SecretModel = require('../resources/db/models/Secret')
+
 module.exports.create = async (event, context) => {
   context.callbackWaitsForEmptyEventLoop = false
 
-  try {
+  const { id: secretId } = event.pathParameters
+  const { name, email } = JSON.parse(event.body)
+  const externalId = uuidv4()
+  const validateUniqueEmailInEvent = { $ne: email }
 
+  try {
+    const result = await SecretModel.updateOne(
+      {
+        externalId: secretId,
+        'participants.email': validateUniqueEmailInEvent
+      },
+      {
+        $push: {
+          participants : {
+            externalId,
+            name,
+            email
+          }
+        }
+      }
+    )
+
+    if (!result.nModified) {
+      throw new Error()
+    }
+
+    return {
+      statusCode: 201,
+      body: JSON.stringify({
+        success: true,
+        id: externalId
+      })
+    }
   } catch (error) {
     return {
       statusCode: 500,
@@ -16,8 +53,31 @@ module.exports.create = async (event, context) => {
 module.exports.delete = async (event, context) => {
   context.callbackWaitsForEmptyEventLoop = false
 
-  try {
+  const { id: secretId, participantId } = event.pathParameters
+  const adminKey = event.headers['admin-key']
 
+  try {
+    const result = await SecretModel.updateOne(
+      {
+        externalId: secretId,
+        adminKey,
+      },
+      {
+        $pull: {
+          participants: {
+            externalId: participantId,
+          }
+        }
+      }
+    )
+
+    if (!result.nModified) {
+      throw new Error()
+    }
+
+    return {
+      statusCode: 204,
+    }
   } catch (error) {
     return {
       statusCode: 500,
